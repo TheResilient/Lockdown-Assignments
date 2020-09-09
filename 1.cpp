0d187d6c697af10c9815c727981ea7869e71d76b
007Patrik <54789620+Pratik282001@users.noreply.github.com>
2020-09-09 18:18:22 +0530

    Add files via upload
    added dsa assignment 2 with infix to postfix conversion and evaluation

---
+++ 1.cpp
@@ -0,0 +1,167 @@
+// C++ program to evaluate value of a postfix expression 
+#include <limits.h>
+#include <iostream> 
+#include <string.h> 
+
+using namespace std; 
+
+// Stack type 
+struct Stack 
+{ 
+	int top; 
+	unsigned capacity; 
+	int* array; 
+}; 
+
+// Stack Operations 
+
+
+int isEmpty(struct Stack* stack) 
+{ 
+	return stack->top == -1 ; 
+} 
+
+char peek(struct Stack* stack) 
+{ 
+	return stack->array[stack->top]; 
+} 
+
+char pop(struct Stack* stack) 
+{ 
+	if (!isEmpty(stack)) 
+		return stack->array[stack->top--] ; 
+	return '$'; 
+} 
+
+void push(struct Stack* stack, char op) 
+{ 
+	stack->array[++stack->top] = op; 
+} 
+
+int peek1(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top]; 
+} 
+
+
+int checkIfOperand(char ch) 
+{ 
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'); 
+} 
+
+
+
+int precedence(char ch) 
+{ 
+    switch (ch) 
+    { 
+    case '+': 
+    case '-': 
+        return 1; 
+
+    case '*': 
+    case '/': 
+        return 2; 
+
+    case '^': 
+        return 3; 
+    } 
+    return -1; 
+}
+
+char covertInfixToPostfix(char *expression)
+{
+    struct Stack *stack = new (Stack);
+    stack->capacity = strlen(expression);
+    stack->top = -1;
+    stack->array = new (int);
+    int i, j;
+    for (i = 0, j = -1; expression[i]; ++i)
+    {
+        // Here we are checking is the character we scanned is operand or not
+        // and this adding to to output.
+        if (checkIfOperand(expression[i]))
+            expression[++j] = expression[i];
+
+        // Here, if we scan character ‘(‘, we need push it to the stack.
+        else if (expression[i] == '(')
+            push(stack, expression[i]);
+
+        // Here, if we scan character is an ‘)’, we need to pop and print from the stack
+        // do this until an ‘(‘ is encountered in the stack.
+        else if (expression[i] == ')')
+        {
+            while (!isEmpty(stack) && peek1(stack) != '(')
+                expression[++j] = pop(stack);
+            if (!isEmpty(stack) && peek1(stack) != '(')
+                return -1; // invalid expression
+            else
+                pop(stack);
+        }
+        else // if an opertor
+        {
+            while (!isEmpty(stack) && precedence(expression[i]) <= precedence(peek(stack)))
+                expression[++j] = pop(stack);
+            push(stack, expression[i]);
+        }
+    }
+    while (!isEmpty(stack))
+        expression[++j] = pop(stack);
+
+    expression[++j] = '\0';
+    printf("%s", expression);
+}
+
+
+
+// The main function that returns value of a given postfix expression 
+int evaluatePostfix(char* exp) 
+{ 
+  	struct Stack* stack = new Stack(); 
+	stack->top = -1; 
+	stack->capacity = strlen(exp); 
+	stack->array = (int*) malloc(stack->capacity * sizeof(int)); 
+
+	
+  	
+	// Create a stack of capacity equal to expression size 
+	// See if stack was created successfully 
+	
+
+	// Scan all characters one by one 
+	for (int i = 0; exp[i]; ++i) 
+	{ 
+		// If the scanned character is an operand (number here), 
+		// push it to the stack. 
+		if (isdigit(exp[i])) 
+			push(stack, exp[i] - '0'); 
+
+		// If the scanned character is an operator, pop two 
+		// elements from stack apply the operator 
+		else
+		{ 
+			int val1 = pop(stack); 
+			int val2 = pop(stack); 
+			switch (exp[i]) 
+			{ 
+			case '+': push(stack, val2 + val1); break; 
+			case '-': push(stack, val2 - val1); break; 
+			case '*': push(stack, val2 * val1); break; 
+			case '/': push(stack, val2/val1); break; 
+			} 
+		} 
+	} 
+	return pop(stack); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+  	char expression[] = "a+b-e*(h+r)";
+  	cout<< covertInfixToPostfix(expression)<<endl;
+	char exp[] = "231*+9-"; 
+	cout<<"postfix evaluation: "<< evaluatePostfix(exp); 
+	
+} 
