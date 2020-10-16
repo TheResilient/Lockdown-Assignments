f5dfafb5a72c969caa0e5a0b0ff028e0f8e312fb
007Patrik <54789620+Pratik282001@users.noreply.github.com>
2020-10-16 12:39:50 +0530

    updated the assignment

--- vehicle.java
+++ vehicle.java
@@ -7,32 +7,32 @@ for all these functionalities in their own class in their own way.
 */
 
 interface gear{
-    void gearchange();
-    void speedup();
+    void gearchange(int i);
+    void speedup(int i);
     void applybrakes();
 }
 
 class Bicycle implements gear{
-    public void gearchange(){System.out.println("changing gears of bicycle !\n");}
-    public void speedup(){System.out.println("speeding up bicycle !\n");}
+    public void gearchange(int i){System.out.println("changing gear to "+i+" of bicycle !\n");}
+    public void speedup(int j){System.out.println("speeding up bicycle to "+j+"!\n");}
     public void applybrakes(){System.out.println("applying brakes of bicycle !\n");}
 }
 class Car implements gear{
-    public void gearchange(){System.out.println("changing gears of Car !\n");}
-    public void speedup(){System.out.println("speeding up Car !\n");}
+    public void gearchange(int i){System.out.println("changing gear to "+i+" of car !\n");}
+    public void speedup(int j){System.out.println("speeding up car to "+j+"!\n");}
     public void applybrakes(){System.out.println("applying brakes of Car !\n");}
 }
 class Bike implements gear{
-    public void gearchange(){System.out.println("changing gears of Bike !\n");}
-    public void speedup(){System.out.println("speeding up Bike !\n");}
+    public void gearchange(int i){System.out.println("changing gear to "+i+" of bike !\n");}
+    public void speedup(int j){System.out.println("speeding up bike to "+j+"!\n");}
     public void applybrakes(){System.out.println("applying brakes of Bike !\n");}
 }
 
-public class vehicle{
+public class Main{
     public static void main(String[] args) {
         gear g=new Car();
-        g.gearchange();
+        g.gearchange(3);
         g.applybrakes();
-        g.speedup();
+        g.speedup(34);
     }
 }
